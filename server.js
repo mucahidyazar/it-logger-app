32509eb4f5d6722a50af194737d4deaefacb6d93
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-13 17:07:38 +0300

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(express.json({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("It Logger");
+});
 app.use("/api/logs", require("./routes/logRoute"));
 app.use("/api/techs", require("./routes/technicianRoute"));
 
